0b708ce94a9d610bd9a8bb73cf2c74b71a890753
Mungai Gikure <josephmungai445@gmail.com>
2025-04-29 19:16:01 +0300

    logs

--- users/api/serializers/users.py
+++ users/api/serializers/users.py
@@ -32,4 +32,4 @@ class GoogleResponseSerializer(serializers.Serializer):
     """
     Serializer for Google Response
     """
-    code = serializers.CharField(required=True, max_length=1000)
+    code = serializers.CharField(required=True)
