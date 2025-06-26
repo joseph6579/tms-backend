cf1ecf83c3344d5b249cef2871010a65b8793738
Mungai Gikure <josephmungai445@gmail.com>
2025-06-26 23:07:38 +0300

    check user existance first

--- deployment/postgres/init/db_init.sh
+++ deployment/postgres/init/db_init.sh
@@ -7,8 +7,8 @@ until pg_isready -U "$POSTGRES_USER"; do
   sleep 2
 done
 
-# Create the database and user if not exists
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+# Create user and database only if they don't exist
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
   DO \$\$
   BEGIN
     IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '${DB_USER}') THEN
@@ -17,6 +17,13 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
   END
   \$\$;
 
-  CREATE DATABASE ${DB_NAME} OWNER ${DB_USER};
+  DO \$\$
+  BEGIN
+    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = '${DB_NAME}') THEN
+      CREATE DATABASE ${DB_NAME} OWNER ${DB_USER};
+    END IF;
+  END
+  \$\$;
+
   GRANT ALL PRIVILEGES ON DATABASE ${DB_NAME} TO ${DB_USER};
 EOSQL
