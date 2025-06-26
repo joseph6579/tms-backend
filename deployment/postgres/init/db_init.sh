5f3cc3078851f08b34f0d5b859696a16050da034
Mungai Gikure <josephmungai445@gmail.com>
2025-06-26 22:53:59 +0300

    add logs

--- deployment/postgres/init/db_init.sh
+++ deployment/postgres/init/db_init.sh
@@ -1,6 +1,22 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 
-# Create the database
-psql -U postgres -c "CREATE USER $DB_USER PASSWORD '$DB_PASSWORD'"
-psql -U postgres -c "CREATE DATABASE $DB_NAME OWNER $DB_USER"
-psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER"
+# Wait until PostgreSQL is ready
+until pg_isready -U "$POSTGRES_USER"; do
+  echo "Waiting for postgres..."
+  sleep 2
+done
+
+# Create the database and user if not exists
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+  DO \$\$
+  BEGIN
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '${DB_USER}') THEN
+      CREATE ROLE ${DB_USER} LOGIN PASSWORD '${DB_PASSWORD}';
+    END IF;
+  END
+  \$\$;
+
+  CREATE DATABASE ${DB_NAME} OWNER ${DB_USER};
+  GRANT ALL PRIVILEGES ON DATABASE ${DB_NAME} TO ${DB_USER};
+EOSQL
