82983c0196626830389d0c0cd78361b039368366
Mungai Gikure <josephmungai445@gmail.com>
2025-07-14 18:43:29 +0300

    trip management

--- dispatch/api/serializers/trips.py
+++ dispatch/api/serializers/trips.py
@@ -3,6 +3,7 @@ from dispatch.models import Trip, TripStop
 from dispatch.api.serializers.orders import OrderSerializer
 from users.api.serializers.users import UserMiniSerializer
 
+
 class TripStopSerializer(serializers.ModelSerializer):
     class Meta:
         model = TripStop
@@ -13,6 +14,7 @@ class TripStopSerializer(serializers.ModelSerializer):
         if data.get('status') == 'completed' and not data.get('completed_at'):
             raise serializers.ValidationError("completed_at is required when status is completed")
         return data
+
 
 class TripSerializer(serializers.ModelSerializer):
     stops = TripStopSerializer(many=True, read_only=True)
@@ -28,3 +30,9 @@ class TripSerializer(serializers.ModelSerializer):
         if data.get('status') == 'completed' and not data.get('completed_time'):
             raise serializers.ValidationError("completed_time is required when status is completed")
         return data
+
+
+class TripCreationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Trip
+        fields = '__all__'
